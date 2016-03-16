5609f62060601a4c9a604240150121d32cfee6e7
Ryan Stantz <ryanstantz@gmail.com>
2016-03-16 16:56:24 -0500

    Finished

--- lib/name_generator.rb
+++ lib/name_generator.rb
@@ -2,3 +2,37 @@
 # you've seen elsewhere.
 
 require 'pry'
+
+#removes whitespace from a name and any punctuation, or capitol letters.
+#takes in a name with whitespace, returns name with no spaces and all lowercase.
+#
+
+def downcase(name)
+  name.downcase
+end
+
+def remove_non_letters(name)
+  alpha_name = name.gsub(/[^a-zA-Z]/, "")
+end
+
+def cap_every_other(name)
+  arr = name.split('')
+  empty = []
+  counter = 0
+  arr.map do |x|
+    counter % 2 == 0 ? empty.push(x.upcase) : empty.push(x)
+    counter += 1
+  end
+  empty.join('')
+end
+
+def prepend_append(name)
+  'xX_'.concat(name).concat('_Xx')
+end
+
+def name_generator(name)
+  name = downcase(name)
+  name = remove_non_letters(name)
+  name = cap_every_other(name)
+  name = prepend_append(name)
+end
--- tests/name_generator_test.rb
+++ tests/name_generator_test.rb
@@ -1,5 +1,53 @@
 require_relative '../lib/name_generator.rb'
 
-# Define whatever methods you need first
 
+
+# Define whatever methods you need first
+def assert_equal(actual, expected ,message)
+  puts actual == expected ?  ";)" :  message
+end
+
+def refute_equal(actual, expected ,message)
+  puts !(actual == expected) ? ";)" : message
+end
 # Now run your actual tests, making sure to do output as necessary
+
+#TEST 1
+expected = 'ryanstantz'
+actual = downcase("RYANSTANTZ")
+assert_equal(expected, actual, 'These two values should be equal')
+
+#TEST 2
+expected = 'ryanstantz'
+actual = remove_non_letters("r y   ansta \nntz")
+assert_equal(expected, actual, 'These two values should be equal')
+
+#TEST 3
+expected = 'ryanstantz'
+actual = remove_non_letters("r[y]a1n&s_t@a#ntz")
+assert_equal(expected, actual, 'These two values should be equal')
+
+#TEST 4
+expected = 'RyAnStAnTz'
+actual = cap_every_other('ryanstantz')
+assert_equal(expected, actual, 'These two values should be equal')
+
+#TEST 5
+expected = 'xX_RyAnStAnTz_Xx'
+actual = prepend_append('RyAnStAnTz')
+assert_equal(expected, actual, 'These two values should be equal')
+
+#TEST 6
+expected = 'xX_RyAnStAnTz_Xx'
+actual = name_generator('ryanstantz')
+assert_equal(expected, actual, 'These two values should be equal')
+
+#TEST 7
+expected = 'xX_AsHlEyBrOeScH_Xx'
+actual = name_generator('a1,3,4,&*^*shley    bRoesch')
+assert_equal(expected, actual, 'These two values should be equal')
+
+#TEST 8
+expected = 'xX_AnThOnYwEiNeR_Xx'
+actual = name_generator('anth234523,..,]][[))989ony]wein/////////er]')
+assert_equal(expected, actual, 'These two values should be equal')
